00da5e7221d4cb1b6b3cad307d0152fc7d6f5b57
BrunoGottardoConti <bruno.conti@sou.inteli.edu.br>
2023-08-18 20:36:45 -0300

    atividade

--- exercicio1.py
+++ exercicio1.py
@@ -3,24 +3,24 @@
 
 
 
-
 # Crie a função que será avaliada no exercício aqui
 
-
-
-
+def conta_palavras_unicas(*palavras):
+    # Remove pontuações e caracteres especiais e converte para minúsculo
+    palavras = [''.join(e.lower() for e in palavra if e.isalnum()) for palavra in palavras]
+    
+    # Utiliza o conjunto (set) para identificar palavras únicas
+    palavras_unicas = set(palavras)
+    
+    # Retorna a quantidade de palavras únicas
+    return len(palavras_unicas)
 
 
 
 # Teste a sua função aqui (caso ache necessário)
 
 
-
-
-
-
-
-
+print(conta_palavras_unicas("Escreva", "uma", "função", "que", "uma", "frase", "função"))
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -8,14 +8,20 @@
 
 
 
+def is_anagram(str1, str2):
+    # Remove espaços, pontuações, converte para minúsculo e ordena as letras
+    sorted_str1 = sorted(''.join(e.lower() for e in str1 if e.isalnum()))
+    sorted_str2 = sorted(''.join(e.lower() for e in str2 if e.isalnum()))
 
-
+    # Retorna True se as strings ordenadas forem iguais
+    return sorted_str1 == sorted_str2
 
 
 # Teste a sua função aqui (caso ache necessário)
 
 
-
+print(is_anagram("banana", "banana"))  # Deve retornar True
+print(is_anagram("locura", "doidera"))  # Deve retornar False
 
 
 
